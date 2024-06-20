4f65886cb46d5839fd614b5eda6038bf7782d53f
Srikanta Nagaraja <srnagar@microsoft.com>
2024-06-20 16:48:54 -0700

    Storage explorer spring sample

---
+++ storage-explorer/src/main/java/com/microsoft/aspire/storage/explorer/Main.java
@@ -0,0 +1,23 @@
+package com.microsoft.aspire.storage.explorer;
+
+import com.microsoft.aspire.storage.explorer.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.init();
+        };
+    }
+
+}
